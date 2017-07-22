01d1de8fb5bf5aaa54d6c1110b11d89c45955c7b
Vincent Jacques <vincent@vincent-jacques.net>
2017-07-22 15:14:42 +0200

    Measure test coverage (with bisect_ppx)

--- run_development_cycle.sh
+++ run_development_cycle.sh
@@ -20,11 +20,31 @@ do
     fi
 done
 
-build General.cmxa unit_tests.byte unit_tests.js -I demo demo.byte
+build -package bisect_ppx General.cmxa unit_tests.byte unit_tests.js -I demo demo.byte
 
-_build/src/unit_tests.byte
+echo
+echo "Running unit tests in node.js"
 node _build/src/unit_tests.js
+echo
+
+echo "Running unit tests in byte code"
+rm -f bisect????.out
+_build/src/unit_tests.byte
+echo
+bisect-summary bisect0001.out
+echo
+bisect-ppx-report -html _build/bisect bisect0001.out
+echo "See coverage report in $(pwd)/_build/bisect/index.html"
+echo
+
+echo "Running demo"
 _build/demo/demo.byte
+rm -f bisect????.out
+
+if [ "x$1" == "x--quick" ]
+then
+    exit
+fi
 
 opam pin add --yes --no-action .
 opam reinstall --yes General
