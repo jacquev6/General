2e4a54b8ac8c880b8d469433c371cd696f48b19a
Vincent Jacques <vincent@vincent-jacques.net>
2017-07-22 11:19:57 +0200

    Prove that pervasive operators are not used
    (not used *directly*)

--- demo/demo.ml
+++ demo/demo.ml
@@ -1,5 +1,7 @@
+let (=) = ()
+
 let () = Printf.printf "General.Int.zero: %n\n" General.Int.zero
 
 let () = Printf.printf "General.Int.one: %n\n" General.Int.one
 
-let () = Printf.printf "General.Int.(one = zero): %b\n" General.Int.(one = zero)
+let () = Printf.printf "General.Int.(O.(one = zero)): %b\n" General.Int.(O.(one = zero))
