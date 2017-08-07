96d5155a603f885240d60fbdd24df9196fac29f3
Vincent Jacques <vincent@vincent-jacques.net>
2017-08-07 18:10:25 +0200

    Export interface as seen in utop

--- src/General.mli
+++ src/General.mli
@@ -53,6 +53,7 @@ end
 module Compare: sig
   type t = LT | EQ | GT
 
+  (* @todo Make sure Poly publishes Compare.t as results (currently if publishes Foundations.Compare.t) *)
   module Poly: sig include Traits.Comparable.SP end
 end
 
