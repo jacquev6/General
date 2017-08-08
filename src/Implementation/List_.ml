947c39a9e6c4a1a2963a21fd70ecccec31fcfbab
Vincent Jacques <vincent@vincent-jacques.net>
2017-08-08 17:36:25 +0200

    Extract Stream's FilterMapable tests and use them for List as well

--- src/Implementation/List_.ml
+++ src/Implementation/List_.ml
@@ -68,18 +68,9 @@ module Tests = struct
   let test = "List" >:: [
     (let module T = Traits.Representable.Tests.Make1(Self)(Examples) in T.test);
     (let module T = Traits.Equatable.Tests.Make1(Self)(Examples) in T.test);
+    (let module T = Traits.FilterMapable.Tests.Make1(Self) in T.test);
     "reverse" >: (lazy (check_string_list ~expected:["3"; "2"; "1"] (reverse ["1"; "2"; "3"])));
     "concat" >: (lazy (check_int_list ~expected:[1; 2; 3; 4; 5; 6] (concat [1; 2; 3] [4; 5; 6])));
-    "flat_map" >: (lazy (check_int_list
-      ~expected:[1; 2; 4; 3; 9; 27; 4; 16; 64; 256]
-      (flat_map [1; 2; 3; 4] ~f:(function
-        | 1 -> [1]
-        | 2 -> [2; 4]
-        | 3 -> [3; 9; 27]
-        | 4 -> [4; 16; 64; 256]
-        | _ -> [42]
-      ))
-    ));
     "prepend" >: (lazy (check_int_list ~expected:[1; 2; 3] (prepend 1 [2; 3])));
     "try_head" >: (lazy (check_some_int ~expected:1 (try_head [1; 2; 3])));
     "try_head []" >: (lazy (check_none_int (try_head [])));
@@ -95,7 +86,6 @@ module Tests = struct
     "reduce" >: (lazy (check_int ~expected:4096 (reduce ~f:Int.exponentiate [2; 3; 4])));
     "try_reduce" >: (lazy (check_some_int ~expected:4096 (try_reduce ~f:Int.exponentiate [2; 3; 4])));
     "try_reduce []" >: (lazy (check_none_int (try_reduce ~f:Int.exponentiate [])));
-    "map" >: (lazy (check_string_list ~expected:["1"; "2"; "3"] (map ~f:Int.to_string [1; 2; 3])));
     "iter" >: (lazy (check_int ~expected:4096 (let p = ref 2 in iter ~f:(fun n -> p := Int.exponentiate !p n) [3; 4]; !p)));
   ]
 end
--- src/Implementation/Stream_.ml
+++ src/Implementation/Stream_.ml
@@ -6,57 +6,6 @@ module Tests = struct
   open Testing
 
   let test = "Stream" >:: [
-    "map" >: (lazy (check_int_list ~expected:[2; 7; 4; 0] ([1; 6; 3; -1] |> of_list |> map ~f:((+) 1) |> to_list)));
-    "map_i" >: (lazy (check_int_list ~expected:[1; 7; 5; 2] ([1; 6; 3; -1] |> of_list |> map_i ~f:(fun ~i x -> i + x) |> to_list)));
-    "map_acc" >: (lazy (check_int_list ~expected:[43; 48; 255; 755] ([1; 6; 3; -1] |> of_list |> map_acc ~acc:42 ~f:(fun ~acc x -> (acc * x, x + acc)) |> to_list)));
-    "filter" >: (lazy (check_int_list ~expected:[3; 15; 9] ([1; 3; 4; 15; 9; 7] |> of_list |> filter ~f:(fun x -> x mod 3 = 0) |> to_list)));
-    "filter_i" >: (lazy (check_int_list ~expected:[3; 15] ([3; 4; 15; 9; 7] |> of_list |> filter_i ~f:(fun ~i x -> i mod 2 = 0 && x mod 3 = 0) |> to_list)));
-    "filter_acc" >: (lazy (check_int_list ~expected:[3; 9] ([2; 3; 4; 15; 9; 7] |> of_list |> filter_acc ~acc:42 ~f:(fun ~acc x -> (acc + x, acc mod 2 = 0 && x mod 3 = 0)) |> to_list)));
-    "filter_map" >: (lazy (check_int_list ~expected:[4; 16; 10] ([1; 3; 4; 15; 9; 7] |> of_list |> filter_map ~f:(fun x -> Option.some_if' (x mod 3 = 0) (x + 1)) |> to_list)));
-    "filter_map_i" >: (lazy (check_int_list ~expected:[4; 16] ([3; 4; 15; 9; 7] |> of_list |> filter_map_i ~f:(fun ~i x -> Option.some_if' (i mod 2 = 0 && x mod 3 = 0) (x + 1)) |> to_list)));
-    "filter_map_acc" >: (lazy (check_int_list ~expected:[4; 10] ([2; 3; 4; 15; 9; 7] |> of_list |> filter_map_acc ~acc:42 ~f:(fun ~acc x -> (acc + x, Option.some_if' (acc mod 2 = 0 && x mod 3 = 0) (x + 1))) |> to_list)));
-    "flat_map" >: (lazy (check_int_list
-      ~expected:[1; 2; 4; 3; 9; 27; 4; 16; 64; 256]
-      (
-        [1; 2; 0; 3; 4]
-        |> of_list
-        |> flat_map ~f:(fun x ->
-          IntRange.create x
-          |> IntRange.ToList.map ~f:(fun n -> Int.exponentiate x (n + 1))
-          |> of_list
-        )
-        |> to_list
-      )
-    ));
-    "flat_map_i" >: (lazy (check_int_list
-      ~expected:[1; 3; 9; 6; 36; 216; 8; 64; 512; 4096]
-      (
-        [1; 2; 0; 3; 4]
-        |> of_list
-        |> flat_map_i ~f:(fun ~i x ->
-          IntRange.create x
-          |> IntRange.ToList.map ~f:(fun n -> Int.exponentiate (x + i) (n + 1))
-          |> of_list
-        )
-        |> to_list
-      )
-    ));
-    "flat_map_acc" >: (lazy (check_int_list
-      ~expected:[43; 86; 88; 255; 261; 279; 1012; 1024; 1072; 1264]
-      (
-        [1; 2; 0; 3; 4]
-        |> of_list
-        |> flat_map_acc ~acc:42 ~f:(fun ~acc x ->
-          let acc = acc * (x + 1)
-          and ys =
-            IntRange.create x
-            |> IntRange.ToList.map ~f:(fun n -> acc + Int.exponentiate x (n + 1))
-            |> of_list
-          in
-          (acc, ys)
-        )
-        |> to_list
-      )
-    ));
+    (let module T = Traits.FilterMapable.Tests.Make1(Self) in T.test);
   ]
 end
